f694ce9d18143f54b56de77dfedfd5f50fdd2774
MRPrio <maksudurprio.mp@gmail.com>
2023-08-28 22:00:14 +0600

    nav bar and banner finished

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,17 +1,45 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineMenu } from "react-icons/ai";
+import { BiMenu } from "react-icons/bi";
 import "./Header.css";
 const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleScroll = () => {
+    const scrolledPosition = window.scrollY;
+  
+    if (scrolledPosition > 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+  
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-      <nav className=" navbar-container py-5 px-12 shadow-md md:border-b-2 md:border-black banner-background">
+      <nav
+        className={`navbar-container py-5 px-12 md:border-black shadow-md md:border-b-2 ${
+          scrolled ? "scrolled-navbar" : "banner-background"
+        }`}
+
+        style={{
+          transition: 'background-color 1s ease ', // Add transition property
+          backgroundColor: scrolled ? '#ffffff' : 'rgb(224, 179, 16)', // Set initial background color
+        }}
+      >
         <div className="container mx-auto flex justify-between items-center">
           <Link
             to="/"
@@ -23,10 +51,10 @@ const Header = () => {
 
           <div className="md:hidden">
             <button className="text-white" onClick={toggleDropdown}>
-              <AiOutlineMenu className="mr-1" />
+              <BiMenu className="mr-1 text-black text-3xl" />
             </button>
             {isDropdownOpen && (
-              <div className="absolute bg-white mt-2 p-2 rounded shadow-md right-0">
+              <div className="absolute bg-white mt-2 p-5 font-semibold rounded shadow-md right-0">
                 <Link to="/story" className="block text-black mb-1">
                   Our Story
                 </Link>
@@ -35,16 +63,11 @@ const Header = () => {
           </div>
 
           <div className="hidden md:flex items-center space-x-4">
-            <Link to="/story" className="text-sm font-semibold">Our Story</Link>
+            <Link to="/story" className="text-sm font-semibold">
+              Our Story
+            </Link>
             <button
-              style={{
-                backgroundColor: "black",
-                borderRadius: "20px",
-                padding: "10px 20px",
-                fontFamily: "'Roboto Slab', serif",
-                color: "white",
-                border: "none",
-              }}
+              className={scrolled ? "scrolled-button" : "nav-btn"}
             >
               Get Started
             </button>
