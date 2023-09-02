48a4277fe6a55d65538a077e20976bc20247ecf5
MRPrio <maksudurprio.mp@gmail.com>
2023-09-03 01:44:21 +0600

    go back button implement done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -2,11 +2,18 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { BsGithub,BsEnvelope } from "react-icons/bs";
 import {RxCross2} from 'react-icons/rx'
+import { useNavigate} from 'react-router-dom'
 const SignUp = () => {
+
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1); // This will take you back to the previous page.
+      };
   return (
     <div className="mt-16 py-24 relative m-5 md:w-1/2 md:mx-auto  border-[1px]">
         <div className="absolute top-5 text-xl right-5">
-            <RxCross2/>
+            <RxCross2 className="cursor-pointer" onClick={handleGoBack}/>
         </div>
       <div className="flex flex-col items-center">
         <h3 className="text-2xl font-semibold my-12">Join BlogWave.</h3>
