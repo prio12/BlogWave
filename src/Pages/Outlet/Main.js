edb09f7271e68e3f389b50cf1d3bdb6563bb0f00
MRPrio <maksudurprio.mp@gmail.com>
2023-09-27 21:30:53 +0600

    loading added for userHeader

--- src/Pages/Outlet/Main.js
+++ src/Pages/Outlet/Main.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import Header from '../../shared/Header/Header';
-import {Outlet} from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import UserHeader from '../../shared/Header/UserHeader';
+import Loader from '../../loading/Loader';
 
 const Main = () => {
-    const user = useSelector((state) => state?.user?.user?.uid);
-    return (
-        <div>
-            {/* <Header/> */}
-            {
-                user?<UserHeader/> : <Header/>
-            }
-            <div style={{paddingTop:"80px"}}>
-            <Outlet/>
-            </div>
-        </div>
-    );
+  const user = useSelector((state) => state?.user?.user?.uid);
+  const loading = useSelector((state) => state?.user?.isLoading);
+
+  return (
+    <div>
+      {loading ? (
+        <Loader/> // Render the Loader when loading is true
+      ) : user ? (
+        <UserHeader />
+      ) : (
+        <Header />
+      )}
+      <div style={{ paddingTop: "80px" }}>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
