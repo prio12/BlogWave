dff085c98e2d6a934d09f3ea2ff67b393354dacc
MRPrio <maksudurprio.mp@gmail.com>
2023-09-30 20:58:20 +0600

    posting blog completed on server

--- src/redux/reducers/blogsReducer.js
+++ src/redux/reducers/blogsReducer.js
@@ -1,11 +1,22 @@
-import { POST_BLOGS } from "../actionTypes/actionTypes";
+import { POST_BLOGS, START_LOADING, STOP_LOADING } from "../actionTypes/actionTypes";
 
 const initialState = {
     blogs: [],
+    isLoading:false,
 }
 
 export const blogReducer = (state = initialState, action) =>{
     switch (action.type) {
+        case START_LOADING:
+            return {
+                ...state,
+                isLoading:true,
+            };
+        case STOP_LOADING:
+            return {
+                ...state,
+                isLoading:false,
+            };
         case POST_BLOGS:
             return {
                 ...state,
--- src/redux/thunk/blogs.js
+++ src/redux/thunk/blogs.js
@@ -1,5 +1,28 @@
+import { postBlog } from "../actions/blogActions"
+import { startLoading, stopLoading } from "../actions/userAuthActions"
+
 export const addBlogPost = (post) =>{
     return async (dispatch) =>{
-        console.log(post);
+        try {
+            dispatch(startLoading())
+            const response = await fetch('http://localhost:5000/blogs',{
+                method:"POST",
+                body:JSON.stringify(post),
+                headers: {
+                    'Content-Type': 'application/json'
+                  }
+            })
+            const data = await response.json();
+            if (data.acknowledged) {
+                dispatch(postBlog({
+                    _id:data.insertedId,
+                    ...post
+                }))
+                dispatch(stopLoading())
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
