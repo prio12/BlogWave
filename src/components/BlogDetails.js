69b5e60d77a57d45593e309669f7b6f74a500964
MRPrio <maksudurprio.mp@gmail.com>
2023-10-10 03:40:00 +0600

    fixed navbar in blog details

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,83 +1,125 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {fetchSelectedBLogData} from '../redux/thunk/blogs'
+import { fetchSelectedBLogData } from "../redux/thunk/blogs";
 import Loader from "../loading/Loader";
-import {PiHandsClappingLight} from 'react-icons/pi'
-import {FaRegComment} from 'react-icons/fa'
-import {MdOutlineBookmarkAdd} from "react-icons/md"
+import { PiHandsClappingLight } from "react-icons/pi";
+import { FaRegComment } from "react-icons/fa";
+import { MdOutlineBookmarkAdd } from "react-icons/md";
 import { useNavigate, useParams } from "react-router-dom";
-import {GrLinkPrevious} from 'react-icons/gr'
-import {FiMoreHorizontal} from 'react-icons/fi'
+import { GrLinkPrevious } from "react-icons/gr";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { fetchUserUpdatedData } from "../redux/thunk/userAuth";
 
 const BlogDetails = () => {
-  const dispatch  = useDispatch();
-  const {id} = useParams();
-  useEffect(() =>{
-    dispatch(fetchSelectedBLogData(id))
-  },[dispatch,id])
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(fetchSelectedBLogData(id));
+  }, [dispatch, id]);
 
   const selectedBlogData = useSelector((state) => state?.blogs?.selectedBlog);
   const user = useSelector((state) => state?.user?.user?.uid);
   const isLoading = useSelector((state) => state?.blogs?.isLoading);
-  const [isOpen, setIsOpen] = useState(false)
-  const handleMoreOptionModal = (data) =>{
-    setIsOpen(!isOpen)
-  }
+  useEffect(() => {
+    dispatch(fetchUserUpdatedData(user));
+  }, [dispatch, user]);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleMoreOptionModal = (data) => {
+    setIsOpen(!isOpen);
+  };
   console.log(isOpen);
   const navigate = useNavigate();
   if (!selectedBlogData || isLoading) {
-    return <Loader/>
+    return <Loader />;
   }
-  const {author,authorImage,title,description,category,date,image,userUid} = selectedBlogData;
-  const handleNavigate = () =>{
-    navigate(-1)
-  }
-  
+  const {
+    author,
+    authorImage,
+    title,
+    description,
+    category,
+    date,
+    image,
+    userUid,
+  } = selectedBlogData;
+  const handleNavigate = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="p-5">
+      {/* sidebar */}
+      {/* sideBar */}
       <div className="w-full md:w-1/2 mx-auto">
         <h2 className="text-4xl font-extrabold my-3">{title}</h2>
         <div className="flex gap-2 items-center my-3">
-            <img src={authorImage} className="h-12 w-12" alt="" />
-            <div>
-            <h5 style={{fontSize:"12px"}} className="font-semibold">{author}</h5>
-            <small style={{fontSize:"10px"}} className="mr-3">7 min read</small>
-            <small style={{fontSize:"10px"}}>12th August</small>
-            </div>
+          <img src={authorImage} className="h-12 w-12" alt="" />
+          <div>
+            <h5 style={{ fontSize: "12px" }} className="font-semibold">
+              {author}
+            </h5>
+            <small style={{ fontSize: "10px" }} className="mr-3">
+              7 min read
+            </small>
+            <small style={{ fontSize: "10px" }}>12th August</small>
+          </div>
         </div>
         <div className="flex my-3 justify-between items-center">
-            <div  className="flex text-xs items-center gap-5">
-                {/* <PiHandsClappingLight className="cursor:pointer "/><span>143</span> */}
-                <div className="flex  items-center gap-1">
-                  <PiHandsClappingLight className="cursor-pointer"/>
-                  <span>143</span>
+          <div className="flex text-xs items-center gap-5">
+            {/* <PiHandsClappingLight className="cursor:pointer "/><span>143</span> */}
+            <div className="flex  items-center gap-1">
+              <PiHandsClappingLight className="cursor-pointer" />
+              <span>143</span>
+            </div>
+            {/* <FaRegComment/><span>5</span> */}
+            <div className="flex items-center gap-1">
+              <FaRegComment className="cursor-pointer" />
+              <span>5</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <div className="relative">
+              {user === userUid && (
+                <FiMoreHorizontal
+                  onClick={handleMoreOptionModal}
+                  title="More"
+                  className="cursor-pointer"
+                />
+              )}
+              {isOpen && (
+                <div
+                  style={{ fontSize: "10px" }}
+                  className="absolute z-10 right-5 bg-white border shadow-lg  p-5 w-32"
+                >
+                  {/* <label
+                    style={{ fontSize: "10px" }}
+                    htmlFor="my-drawer-4"
+                    className="drawer-button btn btn-sm"
+                  >
+                    Edit Story
+                  </label> */}
+                  <button
+                    style={{ fontSize: "10px" }}
+                    className="btn btn-sm my-2"
+                  >
+                    Delete
+                  </button>
                 </div>
-                {/* <FaRegComment/><span>5</span> */}
-                <div className="flex items-center gap-1">
-                  <FaRegComment className="cursor-pointer"/>
-                  <span>5</span> 
-                </div>
+              )}
             </div>
-           <div className="flex items-center gap-3">
-           <div className="relative">
-            {
-              user === userUid && <FiMoreHorizontal onClick={handleMoreOptionModal} title="More" className="cursor-pointer"/>
-            }
-            {
-            isOpen && <div style={{fontSize:"10px"}} className="absolute z-10 right-5 bg-white border shadow-lg  p-5 w-32">
-              <p className="my-2">Edit Picture</p>
-              <p className="my-2">Edit title</p>
-              <p className="my-2">Edit Description</p>
-              <p className="my-2">Delete Blog</p>
-            </div>
-           }
-            </div>
-            <MdOutlineBookmarkAdd title="Add to bookmark" className="cursor-pointer"/>
-           </div>
+            <MdOutlineBookmarkAdd
+              title="Add to bookmark"
+              className="cursor-pointer"
+            />
+          </div>
         </div>
         <img src={image} style={{ height: "50%" }} alt="" />
         <p className="my-5 border border-dark p-2">{description}</p>
-        <GrLinkPrevious className="cursor-pointer" onClick={handleNavigate} title="See more blogs"/>
+        <GrLinkPrevious
+          className="cursor-pointer"
+          onClick={handleNavigate}
+          title="See more blogs"
+        />
       </div>
     </div>
   );
