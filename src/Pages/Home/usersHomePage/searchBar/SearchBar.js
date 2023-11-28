ab7cec693a4eb479e37011fdfc3708105a81b4bb
MRPrio <maksudurprio.mp@gmail.com>
2023-11-28 23:09:53 +0600

    getting key press content for search bar

--- src/Pages/Home/usersHomePage/searchBar/SearchBar.js
+++ src/Pages/Home/usersHomePage/searchBar/SearchBar.js
@@ -3,6 +3,17 @@ import { BsSearch } from "react-icons/bs";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [query, setQuery] = useState('');
+  console.log(query);
+  const handleSearch = () =>{
+    console.log(query)
+  }
+
+  const handleKeyPress = (e) =>{
+    if (e.key === "Enter") {
+      handleSearch()
+    }
+  }
   return (
     <div>
       <div className="flex gap-2 items-center">
@@ -11,11 +22,10 @@ const SearchBar = () => {
           className="text-sm font-semibold focus:outline-none"
           type="text"
           placeholder="Search BlogWave"
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={handleKeyPress}
         />
       </div>
-
-      {/* Topics Slider */}
-
     </div>
 
     
