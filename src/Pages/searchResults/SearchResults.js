39580b46e6f98cc7b29b5f0dcc094012a968a915
MRPrio <maksudurprio.mp@gmail.com>
2023-12-05 18:07:20 +0600

    toggle for selected result done

--- src/Pages/searchResults/SearchResults.js
+++ src/Pages/searchResults/SearchResults.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchResults = () => {
+    const [selectedResult,setSelectedResult] = useState("stories")
+    console.log(selectedResult);
     return (
-        <div>
-            fuck
+        <div className='p-5 md:px-12'>
+           <h1 className='text-4xl font-bold'><span className='text-slate-400'>Results for</span> full</h1>
+           <div className='flex gap-3 items-center text-xs font-semibold my-5 cursor-pointer'>
+            <p onClick={() => setSelectedResult("stories")}>Stories</p>
+            <p onClick={() => setSelectedResult("people")}>People</p>
+           </div>
         </div>
     );
 };
