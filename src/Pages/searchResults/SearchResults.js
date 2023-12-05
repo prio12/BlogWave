df1ab4f21b405109348bcadecac03e63e07d4824
MRPrio <maksudurprio.mp@gmail.com>
2023-12-05 18:12:13 +0600

    rendering and indicating depending on selectedResult

--- src/Pages/searchResults/SearchResults.js
+++ src/Pages/searchResults/SearchResults.js
@@ -7,9 +7,18 @@ const SearchResults = () => {
         <div className='p-5 md:px-12'>
            <h1 className='text-4xl font-bold'><span className='text-slate-400'>Results for</span> full</h1>
            <div className='flex gap-3 items-center text-xs font-semibold my-5 cursor-pointer'>
-            <p onClick={() => setSelectedResult("stories")}>Stories</p>
-            <p onClick={() => setSelectedResult("people")}>People</p>
+            <p onClick={() => setSelectedResult("stories")} className={`${selectedResult === "stories" && "underline"}`}>Stories</p>
+            <p onClick={() => setSelectedResult("people")} className={`${selectedResult === "people" && "underline"}`}>People</p>
            </div>
+           {
+            selectedResult === "stories"? 
+            <div>
+            <p>Stories</p>
+           </div> :
+           <div>
+           <p>people</p>
+          </div>
+           }
         </div>
     );
 };
