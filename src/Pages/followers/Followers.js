4948dd70f8d9704f512a621ac3bd4108bf3256ab
MRPrio <maksudurprio.mp@gmail.com>
2023-12-14 23:20:19 +0600

    fixed the btn conditionllay (follow, unfollow, null)

--- src/Pages/followers/Followers.js
+++ src/Pages/followers/Followers.js
@@ -8,13 +8,12 @@ import StaffPicks from "../Home/usersHomePage/staffPicksBlogs/StaffPicks";
 const Followers = () => {
   const userUid = useSelector((state) => state?.user?.user?.uid);
   const currentUser = useSelector((state) => state?.user?.userData);
-//   console.log(currentUser?.followers);
-//   const [visitProfile,setVisitProfile] = useState(null)
-const [user,setUser] = useState(null)
- 
+  //   console.log(currentUser?.followers);
+  const [visitProfile, setVisitProfile] = useState(null);
+
   const [previousPage, setPreviousPage] = useState("");
   const location = useLocation();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
     const previousPage = location.state?.from;
     setPreviousPage(previousPage);
@@ -32,72 +31,112 @@ const [user,setUser] = useState(null)
     dispatch(getAllUsers());
   }, [dispatch]);
 
-  useEffect(() =>{
-    if (previousPage === '/profile') {
-        setUser(currentUser)
-    }
-    else {
-        const storedUser = JSON.parse(sessionStorage.getItem('user'));
-        setUser(storedUser)
-    }
-  },[previousPage,currentUser])
-//   console.log(user);
+  useEffect(() => {
+    const storedUser = JSON.parse(sessionStorage.getItem("user"));
+    setVisitProfile(storedUser);
+  }, []);
+
+  let content;
+
+  if (previousPage === "/profile") {
+    content = currentUser?.followers.map((follower) => (
+      <div className="flex justify-between my-5 md:pr-12 items-center ">
+        <div className="md:flex lg:flex items-center gap-5">
+          <img src={follower?.profilePic} className="h-16 w-16" alt="" />
+          <div className="my-3 md:my-0">
+            <p className="text-xs font-bold">{follower?.name}</p>
+            {follower.about && (
+              <p className="text-xs">{follower.about.slice(0, 100)}...</p>
+            )}
+          </div>
+        </div>
+        {currentUser?.uid === follower.uid ? (
+          <p>I can't follow myself</p>
+        ) : currentUser?.following?.find(
+            (following) => following?.uid === follower.uid
+          ) ? (
+          <button
+            className="btn btn-xs mb-12 md:mb-0 lg:mb-0"
+            style={{
+              backgroundColor: "transparent", // Set background to transparent
+              color: "#1A8917", // Set text color to red
+              border: "1px solid #1A8917", // Add a red border
+              textTransform: "none",
+            }}
+          >
+            Unfollow
+          </button>
+        ) : (
+          <button
+            className="btn btn-xs mb-12 md:mb-0 lg:mb-0"
+            style={{
+              backgroundColor: "#1A8917",
+              color: "white",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "#1A8917",
+                textTransform: "none",
+              },
+            }}
+          >
+            Follow
+          </button>
+        )}
+      </div>
+    ));
+  } else {
+    content = visitProfile?.followers.map((follower) => (
+      <div className="flex justify-between my-5 md:pr-12 items-center ">
+        <div className="md:flex lg:flex items-center gap-5">
+          <img src={follower?.profilePic} className="h-16 w-16" alt="" />
+          <div className="my-3 md:my-0">
+            <p className="text-xs font-bold">{follower?.name}</p>
+            {follower.about && (
+              <p className="text-xs">{follower.about.slice(0, 100)}...</p>
+            )}
+          </div>
+        </div>
+        {currentUser?.uid ===
+        follower.uid ? null : currentUser?.following?.find(
+            (following) => following?.uid === follower.uid
+          ) ? (
+          <button
+            className="btn btn-xs mb-12 md:mb-0 lg:mb-0"
+            style={{
+              backgroundColor: "transparent", // Set background to transparent
+              color: "#1A8917", // Set text color to red
+              border: "1px solid #1A8917", // Add a red border
+              textTransform: "none",
+            }}
+          >
+            Unfollow
+          </button>
+        ) : (
+          <button
+            className="btn btn-xs mb-12 md:mb-0 lg:mb-0"
+            style={{
+              backgroundColor: "#1A8917",
+              color: "white",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "#1A8917",
+                textTransform: "none",
+              },
+            }}
+          >
+            Follow
+          </button>
+        )}
+      </div>
+    ));
+  }
+
   return (
     <div className="p-5 md:px-12 grid grid-cols-1 gap-5 md:grid-cols-3">
-        <div className="md:col-span-2">
-           {
-            user?.followers.map((follower) =>(
-                <div className="flex justify-between my-5 md:pr-12 items-center ">
-                <div className="md:flex lg:flex items-center gap-5">
-                <img src={follower?.profilePic} className="h-16 w-16" alt="" />
-                <div className="my-3 md:my-0">
-                  <p className="text-xs font-bold">{follower?.name}</p>
-                  {follower.about && (
-                    <p className="text-xs">{follower.about.slice(0, 100)}...</p>
-                  )}
-                </div>
-                </div>
-                {
-                    user?.followers.find((follower) => follower.uid === currentUser.uid) ? (
-                        null
-                    ) : currentUser?.following?.find((following) => following?.uid === follower?.uid) ? (
-                        <button
-                //   onClick={handleUnfollowBtn}
-                  className="btn btn-xs mb-12 md:mb-0 lg:mb-0"
-                    style={{
-                      backgroundColor: "transparent", // Set background to transparent
-                      color: "#1A8917", // Set text color to red
-                      border: "1px solid #1A8917", // Add a red border
-                      textTransform: "none",
-                    }}
-                  >
-                    Unfollow
-                  </button>
-                    ) : (
-                        <button
-                    // onClick={handleFollowBtn}
-                    className="btn btn-xs mb-12 md:mb-0 lg:mb-0"
-                    style={{
-                      backgroundColor: "#1A8917",
-                      color: "white",
-                      textTransform: "none",
-                      "&:hover": {
-                        backgroundColor: "#1A8917",
-                        textTransform: "none",
-                      },
-                    }}
-                  >
-                    Follow
-                  </button>
-                    )
-                }
-              </div>
-            ))
-           }
-        </div>
-        <div className="md:block hidden col-span-1 ">
-            <StaffPicks/>
-        </div>
+      <div className="md:col-span-2">{content}</div>
+      <div className="md:block hidden col-span-1 ">
+        <StaffPicks />
+      </div>
     </div>
   );
 };
