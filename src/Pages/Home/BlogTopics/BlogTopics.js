a9f4097808404f6843c20002dddde709a34c01bd
MRPrio <maksudurprio.mp@gmail.com>
2023-12-19 22:56:06 +0600

    rendering blog categories on BlogTopics component when the user is not logged in

--- src/Pages/Home/BlogTopics/BlogTopics.js
+++ src/Pages/Home/BlogTopics/BlogTopics.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const BlogTopics = () => {
-    return (
-        <div>
-            <h5 style={{fontSize:"13px"}} className='font-bold mb-4'>Discover more of what matters to you</h5>
-            <div style={{fontSize:"12px"}} className="flex flex-wrap gap-2">
-                <p>Programming</p>
-                <p>Writing</p>
-                <p>Technology</p>
-                <p>Programming</p>
-                <p>Writing</p>
-                <p>Technology</p>
-            </div>
-            <p  style={{color:"rgb(44, 148, 44)" ,fontSize:"12px"}} className=' font-semibold'><Link><small>See more Topics</small></Link></p>
+  const blogs = useSelector((state) => state?.blogs?.blogs);
+  const uniqueCategory = new Set();
+
+  if (blogs && blogs.length) {
+    blogs.map((blog) => uniqueCategory.add(blog.category));
+  }
+
+  return (
+    <div>
+      <h5 style={{ fontSize: "13px" }} className="font-bold mb-4">
+        Discover more of what matters to you
+      </h5>
+      <Link to="/signUp">
+        <div style={{ fontSize: "12px" }} className="grid grid-cols-3">
+          {uniqueCategory &&
+            Array.from(uniqueCategory)
+              .slice(0, 9)
+              .map((category) => <p key={category}>{category}</p>)}
         </div>
-    );
+      </Link>
+      <p
+        style={{ color: "rgb(44, 148, 44)", fontSize: "12px" }}
+        className=" font-semibold"
+      >
+        <Link to="/signUp">
+          <small>See more Topics</small>
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default BlogTopics;
